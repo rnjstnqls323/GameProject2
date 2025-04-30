0f1b8a348f02cf05965c4108ffdfb90ecdaf6cc6
jinju785 <wlswn785@nate.com>
2025-04-30 17:51:34 +0900

    new

--- _0429/_0429/Hero.cpp
+++ _0429/_0429/Hero.cpp
@@ -22,11 +22,23 @@ void Hero::Fight(Character* target)
     int num;
     cout << "1.스킬 사용, 2.기본 공격 " << endl;
     cin >> num;
-    if (num == 1) {
-        data.mp -= 30;
+    if (num == 1)
+    {
+        if (GetData().mp < 30)
+        {
+            cout << "마나가 부족합니다. 기본 공격 실행!" << endl;
+            Character::Fight(target);
+            return;
+        }
+
+        SetData(this, Mp, -30);
+        cout << "[내려찍기] 사용을 했습니다" << endl;
         return;
     }
-    else
-        Character::Fight;
-
+    
+    else if (num == 2)
+    {
+        Character::Fight(target);  
+        return;
 }
+
